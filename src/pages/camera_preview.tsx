d606885de25c4a7c3b5f091244bd78ed98a47818
RickyTicky <Rickyabrooks92@gmail.com>
2023-03-18 20:44:23 -0400

    accessing mobile cam

--- src/pages/camera_preview.tsx
+++ src/pages/camera_preview.tsx
@@ -5,23 +5,27 @@ const CameraPreview: React.FC = () => {
 
   useEffect(() => {
     if (videoRef.current) {
-      navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then((stream) => {
-          videoRef.current!.srcObject = stream;
-          videoRef.current!.play();
-        })
-        .catch((error) => {
-          console.error("Failed to access camera", error);
-        });
+      if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices
+          .getUserMedia({ video: true })
+          .then((stream) => {
+            if (videoRef.current) {
+              videoRef.current.srcObject = stream;
+              videoRef.current.play();
+            }
+          })
+          .catch((error) => {
+            console.error("Failed to access camera", error);
+          });
+      } else {
+        console.error("getUserMedia not supported");
+      }
     }
   }, []);
 
   return (
     <div>
-      <video ref={videoRef} />
+      <video ref={videoRef} autoPlay playsInline muted />
     </div>
   );
 };
-
-export default CameraPreview;
