12250f119671fc082408d2b877917e891e1f5503
RickyTicky <Rickyabrooks92@gmail.com>
2023-03-23 17:40:29 -0400

    removed useeffect

--- src/pages/camera_preview.tsx
+++ src/pages/camera_preview.tsx
@@ -4,26 +4,6 @@ const CameraPreview: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [facingMode, setFacingMode] = useState<"user" | "environment">("user");
 
-  useEffect(() => {
-    if (videoRef.current) {
-      if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices
-          .getUserMedia({ video: { facingMode } })
-          .then((stream) => {
-            if (videoRef.current) {
-              videoRef.current.srcObject = stream;
-              videoRef.current.play();
-            }
-          })
-          .catch((error) => {
-            console.error("Failed to access camera", error);
-          });
-      } else {
-        console.error("getUserMedia not supported");
-      }
-    }
-  }, [facingMode]);
-
   const handleFacingModeChange = () => {
     setFacingMode(facingMode === "user" ? "environment" : "user");
   };
