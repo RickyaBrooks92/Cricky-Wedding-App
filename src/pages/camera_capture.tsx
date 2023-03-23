3dffa5d33faa3410a69350b87242ecf0f6910b3b
RickyTicky <Rickyabrooks92@gmail.com>
2023-03-23 17:21:44 -0400

    showing image locally

--- src/pages/camera_capture.tsx
+++ src/pages/camera_capture.tsx
@@ -5,43 +5,12 @@ const CameraComponent = () => {
   const videoRef = useRef(null);
 
   const handlePictureClick = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-    videoRef.current.srcObject = stream;
-
     const canvas = document.createElement("canvas");
-    canvas.width = videoRef.current.videoWidth;
-    canvas.height = videoRef.current.videoHeight;
-    canvas
-      .getContext("2d")
-      .drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-
-    const dataURL = canvas.toDataURL();
-    setPicture(dataURL);
-
-    stream.getTracks().forEach((track) => track.stop());
-
-    const clientId = "658bf713084435a";
-    const formData = new FormData();
-    formData.append("image", dataURL.split(",")[1]);
-
-    try {
-      const response = await fetch("https://api.imgur.com/3/image", {
-        method: "POST",
-        headers: {
-          Authorization: `Client-ID ${clientId}`,
-        },
-        body: formData,
-      });
-      const data = await response.json();
-      console.log(data);
-      if (data.success) {
-        console.log("Image uploaded successfully!");
-      } else {
-        console.error(data.data.error);
-      }
-    } catch (error) {
-      console.error(error);
-    }
+    canvas.width = 640;
+    canvas.height = 480;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(videoRef.current, 0, 0, 640, 480);
+    setPicture(canvas.toDataURL("image/jpeg"));
   };
 
   return (
